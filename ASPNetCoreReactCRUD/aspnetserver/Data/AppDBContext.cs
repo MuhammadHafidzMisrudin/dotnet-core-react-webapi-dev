67ec19c6bfcdb33b6aa0239cd52ca92f8b28b624
Muhammad Hafidz <halfpheez92@gmail.com>
2024-01-14 17:46:48 +0800

    updated appdbcontext class to commit migration and create database in sqlite.

--- ASPNetCoreReactCRUD/aspnetserver/Data/AppDBContext.cs
+++ ASPNetCoreReactCRUD/aspnetserver/Data/AppDBContext.cs
@@ -2,9 +2,29 @@
 
 namespace aspnetserver.Data
 {
-    // to configure entity framework for DB.
+    // to configure entity framework for DB in DBContext class.
     internal sealed class AppDBContext : DbContext
     {
         public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder) => dbContextOptionsBuilder.UseSqlite("Data Source=./Data/AppDB.db");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // create a postsToSeed array.
+            Post[] postsToSeed = new Post[6];
+            for (int i = 1; i <= postsToSeed.Length; i++)
+            {
+                // store values in 
+                postsToSeed[i - 1] = new Post
+                {
+                    PostId = i,
+                    Title = $"Post {i}",
+                    Content = $"This is post {i} and it has some very interesting content. I have liked the video and subscribed."
+                };
+            }
+
+            modelBuilder.Entity<Post>().HasData(postsToSeed);
+        }
     }
 }

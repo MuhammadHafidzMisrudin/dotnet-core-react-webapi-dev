fa7f98e431d5fcf5dbb0d6286550dd4bac56249a
Muhammad Hafidz <halfpheez92@gmail.com>
2024-01-14 23:34:55 +0800

    updated program.cs to improve swagger ui.

--- ASPNetCoreReactCRUD/aspnetserver/Program.cs
+++ ASPNetCoreReactCRUD/aspnetserver/Program.cs
@@ -1,20 +1,25 @@
 using aspnetserver.Data;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(swaggerGenOptions =>
+{
+    swaggerGenOptions.SwaggerDoc("v1", new OpenApiInfo { Title = "ASP.NET Core React Web APIs", Version = "v1" });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(swaggerUIOptions =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    swaggerUIOptions.DocumentTitle = "ASP.NET Core React Web APIs";
+    swaggerUIOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "Web APIs serving a simple Post model.");
+    swaggerUIOptions.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
@@ -24,6 +29,6 @@ app.UseHttpsRedirection();
 
 // add endpoint to get all posts.
 app.MapGet("/get-all-posts", async () =>
-    await PostsRepository.GetPostsAsync());
+    await PostsRepository.GetPostsAsync()).WithTags("Posts Endpoints");
 
 app.Run();

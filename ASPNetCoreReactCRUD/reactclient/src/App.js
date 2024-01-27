40edb68ae760d5de0cf7e811de92ea80358185ee
Muhammad Hafidz <halfpheez92@gmail.com>
2024-01-28 02:13:09 +0800

    modified app.js with renderpoststable function and added bootstrap.

--- ASPNetCoreReactCRUD/reactclient/src/App.js
+++ ASPNetCoreReactCRUD/reactclient/src/App.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const renderPostsTable = () =>{
+    return (
+      <div className="table-responsive mt-5">
+        <table className="table table-bordered border-dark">
+          <thead>
+            <tr>
+              <th scope="col">PostId (PK)</th>
+              <th scope="col">Title</th>
+              <th scope="col">Content</th>
+              <th scope="col">CRUD Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Post 1 Title</td>
+              <td>Post 1 Content</td>
+              <td>
+                <button className="btn btn-dark btn-lg mx-3 my-3">UPDATE</button>
+                <button className="btn btn-danger btn-lg">DELETE</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      Hello World!
+    <div className="container">
+      <div className="row min-vh-100">
+        <div className="col d-flex flex-column justify-content-center align-items-center">
+          <h1>Hello React Client Side!</h1>
+
+          {renderPostsTable()}
+        </div>
+      </div>
     </div>
   );
 }
